d9e49226fdedf2ef15f78612172599892dbcf6e9
dev-jijung <68664202+dev-jijung@users.noreply.github.com>
2023-05-07 14:29:16 +0900

    [Bronze II] Title: 유학 금지, Time: 200 ms, Memory: 17496 KB -BaekjoonHub

--- 백준/Bronze/2789. 유학 금지/유학 금지.java
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -2,12 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		sc.close();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
 
-        input = input.replaceAll("[CAMBRIDGE]", "");
-		
+        char[] removeAlpha = "CAMBRIDGE".toCharArray();
+    
+        for (int i = 0 ; i < removeAlpha.length ; i++) {
+            if (input.indexOf(removeAlpha[i]) > -1) {
+                input = input.replace(String.valueOf(removeAlpha[i]), "");
+            }
+        }
+        
+        //input = input.replaceAll("[CAMBRIDGE]", "");
+        
         System.out.println(input);
 	}
 }
